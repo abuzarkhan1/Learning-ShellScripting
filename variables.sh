96016e9aa8b7b6d484d95009b9832ca644046f10
Abuzar <abuzarkhan1242@gmail.com>
2023-09-30 12:01:30 +0500

    Readonly in ShellScripting

--- variables.sh
+++ variables.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
+#-------------------------------------------------------------------
 
 name="Abuzar"
 age=21
 
 echo "My name is $name and age is $age"
 
+#-------------------------------------------------------------------
+
 name="Khan"
 age="20"
 
 echo "My name is $name and age is $age"
 
+#-------------------------------------------------------------------
 
 host_name=$(hostname)
 echo "this is $host_name"
+
+#-------------------------------------------------------------------
+
+readonly college_name="Quaid-e-Azam"
+echo "My college name is $college_name"
+
+college_name="abuzar"
+echo "My college name is $college_name"
+
+
