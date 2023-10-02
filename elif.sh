665dfb3c846e54d572eec65e95aa9ed178c3d663
Abuzar <abuzarkhan1242@gmail.com>
2023-10-02 21:21:47 +0500

    practice

---
+++ elif.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Prompt the user for input
+echo "Enter a number: "
+read num
+
+# Check if the number is positive, negative, or zero
+if [ $num -gt 0 ]; then
+    echo "The number is positive."
+elif [ $num -lt 0 ]; then
+    echo "The number is negative."
+else
+    echo "The number is zero."
+fi
