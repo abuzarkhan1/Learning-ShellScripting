fadc877e4763625141ae776e57af3c4cfcb3381e
Abuzar <abuzarkhan1242@gmail.com>
2023-10-06 17:27:56 +0500

    Arithmetic operation in Shell

---
+++ Function_2.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+echo "----------------------------------------------------"
+echo "       Arithmetic Operation in Functions            "
+echo "----------------------------------------------------"
+
+
+echo "----------------------------------------------------"
+echo "       Addition  in Functions                        "
+echo "----------------------------------------------------"
+
+
+
+addition(){
+    num1=$1
+    num2=$2
+   let sum=$1+$2
+    echo "Sum of $num1 + $num2 = $sum "
+}
+
+addition 10 20
+
+echo "----------------------------------------------------"
+echo "       Subtraction  in Functions                        "
+echo "----------------------------------------------------"
+
+
+Subtraction(){
+    num1=$1
+    num2=$2
+   let sub=$1-$2
+    echo "Subtraction of $num1 + $num2 = $sub "
+}
+
+Subtraction 10 20
+
+
+echo "----------------------------------------------------"
+echo "       Multiplication  in Functions                        "
+echo "----------------------------------------------------"
+
+
+
+Multiplication(){
+    num1=$1
+    num2=$2
+   let mul=$1*$2
+    echo "Multiplication of $num1 * $num2 = $mul "
+}
+
+Multiplication 10 20
+
+
+echo "----------------------------------------------------"
+echo "       Division  in Functions                        "
+echo "----------------------------------------------------"
+
+
+
+Division(){
+    num1=$1
+    num2=$2
+   let div=$1/$2
+    echo "Divison of $num1 / $num2 = $div "
+}
+
+Division 10 20
